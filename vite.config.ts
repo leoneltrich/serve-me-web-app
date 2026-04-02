22a6cd84e285cc5966440cdf0521b1e6e249afc0
Leon Eltrich <leon.eltrich@cloudsnap.cc>
2026-04-02 23:58:00 +0000

    Fixed cors issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,20 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [sveltekit()]
+export default defineConfig(({ mode }) => {
+
+    const env = loadEnv(mode, process.cwd(), '');
+
+    return {
+        plugins: [sveltekit()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: env.BACKEND_API_URL || 'http://localhost:2000',
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, ''),
+                }
+            }
+        }
+    };
 });
